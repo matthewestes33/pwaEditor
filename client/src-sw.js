bcfa933b8dfdbc077da83ae4e9f617449f8c6a46
Matt Estes <me6384@gmail.com>
2022-07-21 13:32:06 -0700

    update webpack.config from 19 mini project

--- client/src-sw.js
+++ client/src-sw.js
@@ -25,18 +25,21 @@ warmStrategyCache({
   strategy: pageCache,
 });
 
-//
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // callback function defines JS and CSS files to be cached
 registerRoute(
+  // Here we define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
   ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
   new StaleWhileRevalidate({
+    // Name of the cache storage.
     cacheName: 'asset-cache',
     plugins: [
+      // This plugin will cache responses with these headers to a maximum-age of 30 days
       new CacheableResponsePlugin({
         statuses: [0, 200],
       }),
     ],
   })
 );
+
